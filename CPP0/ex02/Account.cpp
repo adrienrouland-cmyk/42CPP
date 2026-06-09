192902f0e6971567587aea246fd00327bdd517ff
arouland <arouland@f4r2s7.paris.42.school>
2026-06-09 15:18:45 +0200

    Constructor in ex02

--- CPP0/ex02/Account.cpp
+++ CPP0/ex02/Account.cpp
@@ -6,9 +6,87 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 18:53:12 by arouland          #+#    #+#             */
-/*   Updated: 2026/06/08 18:53:39 by arouland         ###   ########.fr       */
+/*   Updated: 2026/06/09 15:18:18 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+
+int	_nbAccounts = 0;
+int	_totalAmount = 0;
+int	_totalNbDeposits = 0;
+int	_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts),  _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+{
+	this->_nbAccounts++;
+	this->_totalAmount += initial_deposit;
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+// Public constructor
+
+Account::~Account(void)
+{
+	
+}
+
+Account::Account(void)
+{
+
+}
+// Private constructor, for security by design.
+
+int	Account::getNbAccounts(void)
+{
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount(void)
+{
+	return Account::_totalAmount;
+}
+
+int	Account::getNbDeposits(void)
+{
+	return Account::_totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount(void) const
+{
+
+}
+
+void	Account::displayStatus(void) const
+{
+	
+}
+
+void	Account::_displayTimestamp(void)
+{
+	
+}
 
