d372b6cd8f6f87301800beb74f96f4f8615101cb
Adrien <adrienrouland@gmail.com>
2026-06-11 19:39:02 +0200

    Correction ex04 et sécurisation

--- CPP1/ex04/main.cpp
+++ CPP1/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 14:13:23 by arouland          #+#    #+#             */
-/*   Updated: 2026/06/11 15:26:04 by arouland         ###   ########.fr       */
+/*   Updated: 2026/06/11 19:38:29 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,34 @@ int main(int argc, char **argv)
     std::string filename;
     std::string outfile;
     std::string line;
-    std::string word;
     std::string s1;
     std::string s2;
+    size_t pos;
 
     if (argc != 4)
-    {
-        std::cerr << "Wrong number of arguments" << std::endl;
-        return (1);
-    }
+        return (std::cerr << "Wrong number of arguments" << std::endl, 1);
     filename = argv[1];
-    std::ifstream ifs(filename);
-    outfile = filename + ".replace";
-    std::ofstream ofs(outfile);
     s1 = argv[2];
     s2 = argv[3];
+    if (s1.empty())
+        return (std::cerr << "Error: the string to replace canont be empty" << std::endl, 1);
+    std::ifstream ifs(filename.c_str());
+    if (!ifs.is_open())
+        return (std::cerr << "Cannot open " << filename << std::endl, 1);
+    outfile = filename + ".replace";
+    std::ofstream ofs(outfile.c_str());
+    if (!ofs.is_open())
+        return (std::cerr << "Cannot open " << outfile << std::endl, 1);
     while (std::getline(ifs, line))
     {
-        std::stringstream ss(line);
-        while (ss >> word)
+        pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
         {
-            if (word == s1)
-                ofs << s2 << " ";
-            else
-                ofs << word << " ";
+            line.erase(pos, s1.length());
+            line.insert(pos, s2);
+            pos += s2.length();
         }
-        ofs << std::endl;
+        ofs << line << std::endl;
     }
     ifs.close();
     ofs.close();
