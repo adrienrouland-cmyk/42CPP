6401a83cc062fb551448b3126872d0c183f17223
Adrien <adrienrouland@gmail.com>
2026-06-17 13:26:11 +0200

    ex01 ready CPP2

--- CPP2/ex01/Fixed.hpp
+++ CPP2/ex01/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 19:23:48 by arouland          #+#    #+#             */
-/*   Updated: 2026/06/16 19:28:28 by arouland         ###   ########.fr       */
+/*   Updated: 2026/06/17 13:12:33 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H
 #define FIXED_H
+
+#include <iostream>
 
 class Fixed
 {
@@ -20,12 +22,18 @@ class Fixed
 		Fixed(Fixed const &src);
 		~Fixed(void);
 		Fixed &operator=(Fixed const &rhs);
+		Fixed(int const n);
+		Fixed(float const z);
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 	private:
-		int	_nb;
+		int	_value; // valeur multipliée par 256
 		static const int _nbFractBits;
 };
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &rhs);
 
 #endif
