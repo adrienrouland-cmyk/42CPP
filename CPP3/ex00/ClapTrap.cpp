ccfe357cecc6bb2bc1f148bcf23b524a9681b867
arouland <arouland@f4r2s7.paris.42.school>
2026-06-23 17:08:20 +0200

    fix ex00

--- CPP3/ex00/ClapTrap.cpp
+++ CPP3/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 10:49:41 by arouland          #+#    #+#             */
-/*   Updated: 2026/06/18 23:18:53 by arouland         ###   ########.fr       */
+/*   Updated: 2026/06/23 17:06:11 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 ClapTrap::ClapTrap(std::string name) : 
     _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+    std::cout << "ClapTrap " << this->_name << " is born with " << _hitPoints << " hitPoints, " 
+        << _energyPoints << " energyPoints, and " << _attackDamage 
+        << " attackDamage" << std::endl;
+}
+
+ClapTrap::ClapTrap(void) : 
+    _name("default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
     std::cout << "ClapTrap " << this->_name << " is born with " << _hitPoints << " hitPoints, " 
         << _energyPoints << " energyPoints, and " << _attackDamage 
--- CPP3/ex00/main.cpp
+++ CPP3/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 15:37:22 by arouland          #+#    #+#             */
-/*   Updated: 2026/06/18 23:26:23 by arouland         ###   ########.fr       */
+/*   Updated: 2026/06/23 17:07:41 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,11 @@ int main(void)
     trap.takeDamage(11);
     std::cout << "ClapTrap hitpoint = " << trap.getHitPoints() << std::endl;
     trap.beRepaired(20);
-    trap.attack("méchant");    
+    trap.attack("méchant");
+
+	std::cout << std::endl << std::endl;
+
+	ClapTrap trap2("trap2");
+	ClapTrap trap3(trap2);
+	trap3.attack("mechant3");
 }
