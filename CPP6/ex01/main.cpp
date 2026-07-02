928389172ee46060e2b1124b3018e620101380d8
arouland <arouland@f4r2s12.paris.42.school>
2026-07-02 16:15:52 +0200

    fix ex02 et ex01

--- CPP6/ex01/main.cpp
+++ CPP6/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 12:37:33 by arouland          #+#    #+#             */
-/*   Updated: 2026/07/02 12:55:19 by arouland         ###   ########.fr       */
+/*   Updated: 2026/07/02 15:44:36 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int main(void)
 	Data* data = new Data;
 	data->data = 42;
 
-	std::cout << "initial address :" << data << std::endl;
+	std::cout << "initial address : " << data << std::endl;
 	uintptr_t ptr = Serializer::serialize(data);
-	std::cout << "serialize address :" << ptr << std::endl;
+	std::cout << "serialize address : " << ptr << std::endl;
 	Data* newdata = Serializer::deserialize(ptr);
-	std::cout << "deserialized address :" << newdata << std::endl;
+	std::cout << "deserialized address : " << newdata << std::endl;
 	std::cout << "deserialized value : " << newdata->data << std::endl;
 	delete data;
 }
