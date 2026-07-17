2e4dfa61a8ad4faac71138fd0f71da7c8c09f27e
Adrien <adrienrouland@gmail.com>
2026-07-17 16:47:48 +0200

    deleted debug functions

--- CPP9/ex00/BitcoinExchange.hpp
+++ CPP9/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: arouland <arouland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 12:02:28 by arouland          #+#    #+#             */
-/*   Updated: 2026/07/17 16:27:14 by arouland         ###   ########.fr       */
+/*   Updated: 2026/07/17 16:47:31 by arouland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,12 @@ class BitcoinExchange {
 
         bool loadDatabase(const std::string &filename); // load.csv et charge _db;
         void processInput(const std::string &filename); // load la base input.txt
-
-        /* DEBUG */
-        void printDB() const {
-            const std::map<std::string, float> database = this->_database;
-            for (std::map<std::string, float>::const_iterator it = _database.begin();
-            it != _database.end(); ++it)
-            {
-                std::cout << it->first << ":" << it->second << std::endl;
-            }
-        }
-        /*********/
         
-        /* A REMETTRE EN PRIVE */
-        float getRateForDate(const std::string &date) const; // renvoie l'exchange rate pour la date, ou erreur
     private:
         std::map<std::string, float> _database;
         static std::string trim(const std::string &str);
         static bool isValidDate(const std::string &date);
+        float getRateForDate(const std::string &date) const; // renvoie l'exchange rate pour la date, ou erreur
 };
 
 #endif
